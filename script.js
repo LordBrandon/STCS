8df67008cd5a712d99c21d4701664af700530290
LordBrandon <bkw42102@gmail.com>
2019-11-26 20:07:32 +0000

    Majority Changes

---
+++ script.js
@@ -0,0 +1,23 @@
+// Animation for navigation
+
+// Home Page
+/*
+function demoTrans(){
+setTimeout(function(){ 
+window.location.href = 'buyademo.html';
+ }, 2000);
+}
+
+function delayTrans(){
+setTimeout(function(){ 
+window.location.href = 'Infopage.html';
+ }, 2000);
+}
+*/
+
+// Nabeel's Shit
+function confirmInfo() {
+  alert(" Thank you! " + document.forum.name.value + ". We appreciate this Information and will use it to make services much better.");
+
+window.location = '../';
+}
